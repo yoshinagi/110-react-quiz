44b5bd19c0bbaab64c79989a47285d65bf63ef65
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-06-30 22:30:12 +0900

    Add top page

---
+++ src/components/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
+export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    async function quiz() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+
+        if (!res) throw new Error('Error');
+
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+
+    quiz();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+      </Main>
+    </div>
+  );
+}
