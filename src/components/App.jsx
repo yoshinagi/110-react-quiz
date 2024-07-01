d2ab0dcefccae347ceb3686c426b8f63fbb58918
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-07-01 14:19:26 +0900

    Add points function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,16 @@ import Main from './Main';
 import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
+import Question from './Question';
 
 const initialState = {
   questions: [],
 
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -20,13 +24,31 @@ function reducer(state, action) {
     case 'dataFailed':
       return { ...state, status: 'error' };
 
+    case 'start':
+      return { ...state, status: 'active' };
+
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
     default:
       throw new Error('Action unknown');
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -54,7 +76,16 @@ export default function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
---
+++ src/components/Options.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+function Options({ question, dispatch, answer }) {
+  console.log(question);
+
+  const hasAnswered = answer !== null;
+
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? 'answer' : ''} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
+          } `}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+          disabled={hasAnswered}
+          key={option}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
